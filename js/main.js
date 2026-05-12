12dbf953dc1e5edabd41611725b76df989b97fee
bdognieto <bnieto@ucsd.edu>
2026-05-11 18:42:43 -0700

    update chart layout

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ Promise.all([
 
   const width = 800;
   const height = 400;
-  const margin = { top: 40, right: 40, bottom: 50, left: 80 };
+  const margin = { top: 40, right: 120, bottom: 55, left: 90 };
 
   const svg = d3.select("#co2-chart")
     .append("svg")
@@ -37,6 +37,7 @@ Promise.all([
 
   const y = d3.scaleLinear()
     .domain(d3.extent(co2Data, d => d.co2mass))
+    .nice()
     .range([height - margin.bottom, margin.top]);
 
   svg.append("g")
@@ -45,23 +46,65 @@ Promise.all([
 
   svg.append("g")
     .attr("transform", `translate(${margin.left}, 0)`)
-    .call(d3.axisLeft(y));
+    .call(
+      d3.axisLeft(y)
+        .ticks(6)
+        .tickFormat(d => `${(d / 1e15).toFixed(1)}`)
+    );
+
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", height - 10)
+    .attr("text-anchor", "middle")
+    .text("Year");
+
+  svg.append("text")
+    .attr("x", -height / 2)
+    .attr("y", 25)
+    .attr("transform", "rotate(-90)")
+    .attr("text-anchor", "middle")
+    .text("Atmospheric CO₂ Mass (×10¹⁵ kg)");
 
   const line = d3.line()
     .x(d => x(d.year))
     .y(d => y(d.co2mass));
 
   const grouped = d3.group(co2Data, d => d.scenario);
-  
+
+  const color = d3.scaleOrdinal()
+    .domain(["historical", "ssp245", "ssp585"])
+    .range(["#555555", "#f28e2b", "#e15759"]);
+
   svg.selectAll(".co2-line")
     .data(grouped)
     .join("path")
     .attr("class", "co2-line")
     .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 2)
+    .attr("stroke", ([scenario]) => color(scenario))
+    .attr("stroke-width", 2.5)
     .attr("d", ([scenario, values]) => {
       values.sort((a, b) => a.year - b.year);
-       return line(values);
+      return line(values);
     });
+
+  const legend = svg.append("g")
+    .attr("transform", `translate(${width - margin.right + 20}, ${margin.top})`);
+
+  ["historical", "ssp245", "ssp585"].forEach((scenario, i) => {
+    const row = legend.append("g")
+      .attr("transform", `translate(0, ${i * 24})`);
+
+    row.append("line")
+      .attr("x1", 0)
+      .attr("x2", 20)
+      .attr("y1", 0)
+      .attr("y2", 0)
+      .attr("stroke", color(scenario))
+      .attr("stroke-width", 3);
+
+    row.append("text")
+      .attr("x", 28)
+      .attr("y", 5)
+      .text(scenario);
+  });
 });
